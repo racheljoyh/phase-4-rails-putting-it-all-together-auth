603438d6a384f4bb9755c5f6c81af9c2af594b5d
Rachel <racheljhumes@gmail.com>
2023-01-19 10:54:18 -0700

    commit tree 37607514d3484bc6a543604c4cdb53d45bc9e8b0

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  skip_before_action :authorize, only: :create
+
+  def create
+    user = User.find_by(username: params[:username])
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      render json: user
+    else
+      render json: { errors: ["Invalid username or password"] }, status: :unauthorized
+    end
+  end
+
+     def destroy
+        if User.find_by(id: session[:user_id])
+            session.delete :user_id
+            head :no_content
+        else
+            render json: { errors: ["Unable to delete"] }, status: :unauthorized
+        end
+    end
+
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+
+
+  def show
+    user = User.find_by(id: session[:user_id])
+    render json: user
+  end
+
+  def create
+    user = User.create!(user_params)
+    session[:user_id] = user.id
+    render json: user, status: :created
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :password, :password_confirmation, :image_url, :bio)
+  end
+
+end
